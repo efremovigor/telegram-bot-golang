e622d6004c2e66508d8c2e5b6498ce9bc5d0a244
igore <igor.e@alyce.com>
2022-04-24 12:31:38 +0300

    init

--- app/main.go
+++ app/main.go
@@ -94,5 +94,5 @@ func main() {
 		fmt.Println("reply sent")
 		return c.JSON(http.StatusOK, "")
 	})
-	e.Logger.Fatal(e.StartTLS(":443", "./build/domain.crt", "./build/domain.key"))
+	e.Logger.Fatal(e.StartTLS(":443", "./build/domain.pem", "./build/domain.key"))
 }
