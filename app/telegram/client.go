57e73495d47a2f04872d4b4cc3d572e5b28aba00
igore <igor.e@alyce.com>
2022-06-18 22:21:01 +0300

    listener

--- app/telegram/client.go
+++ app/telegram/client.go
@@ -182,7 +182,7 @@ func sendVoice(chatId int, country string, info cambridge.CambridgeInfo, hasMore
 	_ = writer.WriteField("title", title)
 	_ = writer.WriteField("chat_id", strconv.Itoa(chatId))
 	if hasMore {
-		_ = writer.WriteField("reply_markup[keyboard][text][]", NextRequestMessage)
+		_ = writer.WriteField("reply_markup[keyboard][][text]", NextRequestMessage)
 	}
 	err = writer.Close()
 	if err != nil {
