7161984bde0485c6e21710f694272891025fd343
igore <igor.e@alyce.com>
2022-06-18 10:32:22 +0300

    listener

--- app/telegram/listener.go
+++ app/telegram/listener.go
@@ -1,12 +1,21 @@
 package telegram
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 func HandleRequests(listener Listener) {
 	for {
 		select {
 		case request := <-listener.Message:
 			switch request.Type {
 			case "text":
-				println(request.Message)
+				if infoInJson, err := json.Marshal(request); err == nil {
+					fmt.Println(infoInJson)
+				} else {
+					fmt.Println(err)
+				}
 				sendMessage(request.Message.(RequestTelegramText))
 			case "voice":
 				voice := request.Message.(CambridgeRequestTelegramVoice)
