1dae2e9b37c41b9b1bd52297ffc3812b5558d403
igore <igor.e@alyce.com>
2022-06-18 21:53:04 +0300

    listener

--- app/telegram/listener.go
+++ app/telegram/listener.go
@@ -9,6 +9,8 @@ func HandleRequests(listener Listener) {
 	for {
 		select {
 		case request := <-listener.Message:
+			jsonRequest, _ := json.Marshal(request)
+			fmt.Println("from telegram json:" + string(jsonRequest))
 			switch request.Type {
 			case "text":
 				var textRequest RequestTelegramText
